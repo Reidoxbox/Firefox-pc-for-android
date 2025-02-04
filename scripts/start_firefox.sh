356720f7075b247d51792606b1c27ef8060a9cfe
Zulbonks <zulbonks2@hotmail.com>
2025-02-04 14:43:20 -0300

    Atualizar o start_firefox.sh

--- scripts/start_firefox.sh
+++ scripts/start_firefox.sh
@@ -2,3 +2,15 @@
 
 # Iniciar Debian e executar o Firefox
 proot-distro login debian -- bash -c "firefox"
+
+# Criar pasta para o Firefox AppImage
+mkdir -p ~/firefox-appimage && cd ~/firefox-appimage
+
+# Baixar a versão mais recente do Firefox AppImage
+wget -O firefox.appimage "https://github.com/srevinsaju/Firefox-Appimage/releases/latest/download/firefox-x86_64.AppImage"
+
+# Dar permissão de execução
+chmod +x firefox.appimage
+
+# Rodar o Firefox AppImage
+./firefox.appimage
