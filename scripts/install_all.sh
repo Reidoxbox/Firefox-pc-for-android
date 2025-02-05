5b101987513cd689e8b536d244b9029120ce21c7
Zulbonks <zulbonks2@hotmail.com>
2025-02-04 21:24:31 -0300

    Update install_all.sh

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,41 +1,22 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "Iniciando instalação completa do Firefox no Android..."
+echo "Atualizando pacotes do Termux..."
+pkg update -y && pkg upgrade -y
 
-# Definir caminho do diretório do projeto
-PROJECT_DIR="$HOME/Firefox-pc-for-android/scripts"
+echo "Instalando dependências necessárias..."
+pkg install -y proot-distro wget unzip pulseaudio
 
-# Garantir que os scripts são executáveis
-chmod +x $PROJECT_DIR/*.sh
-
-# Instalar Debian no Proot
 echo "Instalando Debian no Proot..."
-bash $PROJECT_DIR/install_debian_proot.sh || { echo "Erro na instalação do Debian!"; exit 1; }
+proot-distro install debian
 
-# Instalar Firefox
+echo "Baixando e instalando o XServer..."
+bash scripts/install_xserver.sh
+
 echo "Instalando o Firefox..."
-bash $PROJECT_DIR/install_firefox.sh || { echo "Erro na instalação do Firefox!"; exit 1; }
+bash scripts/install_firefox.sh
 
-# Instalar Extensões do Firefox
-echo "Instalando Extensões..."
-bash $PROJECT_DIR/install_extensions.sh || { echo "Erro na instalação das extensões!"; exit 1; }
+echo "Instalando extensões no Firefox..."
+bash scripts/install_extensions.sh
+bash scripts/install_extensions_2.sh
 
-bash $PROJECT_DIR/install_xserver.sh || { acho "ERRO, ERRO NO XSERVERRRR!!!"; exit 1; }
-
-# Criar pasta para o Firefox
-mkdir -p ~/firefox-appimage && cd ~/firefox-appimage
-
-# Baixar a versão oficial do Firefox
-wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
-
-# Extrair o arquivo
-tar xjf firefox.tar.bz2
-
-# Mover para um diretório padrão
-mv firefox ~/firefox
-
-# Criar um link simbólico para facilitar a execução
-ln -s ~/firefox/firefox ~/.local/bin/firefox
-
-# Testar se o Firefox abre
-~/firefox/firefox &
+echo "Instalação concluída! Para iniciar, execute: bash scripts/start.sh"
